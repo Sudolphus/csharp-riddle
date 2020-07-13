7846e855ef65678d7c4bb68843b9c58f50c5fcd4
Spencer Moody <spencer.moody@outlook.com>
2020-07-13 14:01:01 -0700

    Use dictionaries to hold riddle questions and answers
    Co-Authored-By: Micheal Hansen <micheal.hansen@gmail.com>

--- Riddles/Sphinx.cs
+++ Riddles/Sphinx.cs
@@ -1,13 +1,19 @@
+using System.Collections.Generic;
+
 namespace Riddle.SphinxRiddler
 {
   public class Sphinx
   {
     public string Answer { get; set; }
 
+    private Dictionary<int, string> riddleQuestions = new Dictionary<int, string>() { {0, "What has 4 legs in the morning, 2 legs in the afternoon, and 3 legs at night?"} };
+
+    private Dictionary<int, string> riddleAnswers = new Dictionary<int, string>() { {0, "A person"} };
+
     public string AskRiddle()
     {
-      string riddle = "What has 4 legs in the morning, 2 legs in the afternoon, and 3 legs at night?";
-      string answer = "A person";
+      string riddle = riddleQuestions[0];
+      string answer = riddleAnswers[0];
       Answer = answer;
       return riddle;
     }
