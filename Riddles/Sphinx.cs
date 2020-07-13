ddd412f126f0d17993d8b245090564fbfc9d4312
Spencer Moody <spencer.moody@outlook.com>
2020-07-13 13:44:36 -0700

    Add functionality to ask question and print to the console
    Co-Authored-By: Micheal Hansen <micheal.hansen@gmail.com>

---
+++ Riddles/Sphinx.cs
@@ -0,0 +1,11 @@
+namespace Sphinx.Riddle
+{
+  public class Sphinx
+  {
+    public string AskRiddle()
+    {
+      string riddle = "What has 4 legs in the morning, 2 legs in the afternoon, and 3 legs at night?";
+      return riddle;
+    }
+  }
+}
