3808d660e0ac3678cf7e6889c110c19d2eca6fa9
Spencer Moody <spencer.moody@outlook.com>
2020-07-13 14:09:10 -0700

    Randomize riddle
    Co-Authored-By: Micheal Hansen <micheal.hansen@gmail.com>

--- Riddles/Sphinx.cs
+++ Riddles/Sphinx.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Riddle.SphinxRiddler
@@ -6,16 +7,23 @@ namespace Riddle.SphinxRiddler
   {
     public string Answer { get; set; }
 
-    private Dictionary<int, string> riddleQuestions = new Dictionary<int, string>() { {0, "What has 4 legs in the morning, 2 legs in the afternoon, and 3 legs at night?"} };
+    private Dictionary<int, string> riddleQuestions = new Dictionary<int, string>() { {0, "What has 4 legs in the morning, 2 legs in the afternoon, and 3 legs at night?"}, {1, "I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?"}, {2, "What can you keep after giving to someone?"} };
 
-    private Dictionary<int, string> riddleAnswers = new Dictionary<int, string>() { {0, "A person"} };
+    private Dictionary<int, string> riddleAnswers = new Dictionary<int, string>() { {0, "A person"}, {1, "Fire"}, {2, "Your word"} };
 
     public string AskRiddle()
     {
-      string riddle = riddleQuestions[0];
-      string answer = riddleAnswers[0];
+      int nextRiddle = GetRandom();
+      string riddle = riddleQuestions[nextRiddle];
+      string answer = riddleAnswers[nextRiddle];
       Answer = answer;
       return riddle;
+    }
+
+    private int GetRandom()
+    {
+      Random rnd = new Random();
+      return rnd.Next(0, 3);
     }
   }
 }
