0fc49c8cf381550ef6d587c8f8f98eef342f6449
Spencer Moody <spencer.moody@outlook.com>
2020-07-13 13:51:20 -0700

    Add AskRiddle method with riddle answers
    Co-Authored-By: Micheal Hansen <micheal.hansen@gmail.com>

--- Riddles/Sphinx.cs
+++ Riddles/Sphinx.cs
@@ -1,10 +1,14 @@
-namespace Sphinx.Riddle
+namespace Riddle.SphinxRiddler
 {
   public class Sphinx
   {
+    public string Answer { get; set; }
+
     public string AskRiddle()
     {
       string riddle = "What has 4 legs in the morning, 2 legs in the afternoon, and 3 legs at night?";
+      string answer = "A person";
+      Answer = answer;
       return riddle;
     }
   }
